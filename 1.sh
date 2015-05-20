6569c62ec37a087ef83ad327ba8dab74b78c68d7
scz2011 <11922803@qq.com>
2015-05-20 13:05:25 +0800

    no message

--- 1.sh
+++ 1.sh
@@ -9,10 +9,27 @@ echo "# Author: tommy sun"
 echo "#"
 echo "#############################################################"
 echo ""
-if [ "$OS" == 'CentOS' ]; then
+# Check OS
+mkdir temp;
+mkdir down;
+mkdir web;
+yum update;
+yum install gcc-c++ make;
+echo "#nodejs#"
+cd temp;
+wget http://nodejs.org/dist/v0.12.3/node-v0.12.3.tar.gz;
+tar xvf node-v0.12.3.tar.gz;
+cd node-v0.12.3
+./configure ;
+make;
+make install;
+echo "#安装 aspnet#"
+npm install -g grunt
+npm install -g yo
+npm install -g yo grunt-cli generator-aspnet bower
 
-	mkdir temp;
-	mkdir down;
-	mkdir web;
-	yum update;
-fi
+cd
+tar zxvf PartsUnlimited-demo-app-linux.tar.gz -C ~/
+source ~/.dnx/dnvm/dnvm.sh
+dnvm use 1.0.0-beta5-11624 -r coreclr -arch x64
+dnx . kestrel
