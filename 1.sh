d5339cfc43aa39bf82a3631ede93635e98ea7669
scz2011 <11922803@qq.com>
2015-05-20 09:42:07 +0800

    no message

--- 1.sh
+++ 1.sh
@@ -1,3 +1,18 @@
+#! /bin/bash
+clear
+echo ""
+echo "#############################################################"
+echo "#Centos 系统初始化"
+echo "# Intro:"
+echo "#"
+echo "# Author: tommy sun"
+echo "#"
+echo "#############################################################"
+echo ""
 if [ "$OS" == 'CentOS' ]; then
 
+	mkdir temp;
+	mkdir down;
+	mkdir web;
+	yum update;
 fi
