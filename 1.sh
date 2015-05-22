ef31dd3991a868308015d64ebab3bb539f9060a3
scz2011 <11922803@qq.com>
2015-05-23 00:33:55 +0800

    no message

--- 1.sh
+++ 1.sh
@@ -10,24 +10,26 @@ echo "#"
 echo "#############################################################"
 echo ""
 # Check OS
-mkdir temp;
-mkdir down;
-mkdir web;
-yum update;
-yum install gcc-c++ make;
+sudo mkdir temp;
+sudo mkdir down;
+sudo mkdir web;
+sudo yum update;
+sudo yum install gcc-c++ make;
 echo "#nodejs#"
 cd temp;
-wget http://nodejs.org/dist/v0.12.3/node-v0.12.3.tar.gz;
-tar xvf node-v0.12.3.tar.gz;
+sudo wget http://nodejs.org/dist/v0.12.3/node-v0.12.3.tar.gz;
+sudo tar zxvf node-v0.12.3.tar.gz;
 cd node-v0.12.3
-./configure ;
-make;
-make install;
+sudo ./configure ;
+sudo make;
+sudo make install;
 echo "#安装 aspnet#"
-npm install -g grunt
-npm install -g yo
-npm install -g yo grunt-cli generator-aspnet bower
+sudo npm install -g grunt
+sudo npm install -g yo grunt-cli generator-aspnet bower
 
+sudo yum install ruby
+sudo yum groupinstall 'Development Tools' && sudo yum install curl git m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
 cd
 tar zxvf PartsUnlimited-demo-app-linux.tar.gz -C ~/
 source ~/.dnx/dnvm/dnvm.sh
