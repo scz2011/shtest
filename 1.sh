082a390947b82ab34b5a258a9b00f0ac44e6bbc4
scz2011 <11922803@qq.com>
2015-05-25 16:14:13 +0800

    no message

--- 1.sh
+++ 1.sh
@@ -15,6 +15,7 @@ sudo mkdir down;
 sudo mkdir web;
 sudo yum update;
 sudo yum install gcc-c++ make;
+sudo yum install php;
 echo "#nodejs#"
 cd temp;
 sudo wget http://nodejs.org/dist/v0.12.3/node-v0.12.3.tar.gz;
@@ -24,14 +25,15 @@ sudo ./configure ;
 sudo make;
 sudo make install;
 echo "#安装 aspnet#"
-sudo npm install -g grunt
-sudo npm install -g yo grunt-cli generator-aspnet bower
+npm install -g grunt
+npm install -g yo grunt-cli generator-aspnet bower
 
 sudo yum install ruby
 sudo yum groupinstall 'Development Tools' && sudo yum install curl git m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-cd
-tar zxvf PartsUnlimited-demo-app-linux.tar.gz -C ~/
-source ~/.dnx/dnvm/dnvm.sh
-dnvm use 1.0.0-beta5-11624 -r coreclr -arch x64
-dnx . kestrel
+echo "#安装 Lumen#"
+yum install php
+sudo curl -sS https://getcomposer.org/installer | php
+
+php composer.phar
+
+sudo composer global require "laravel/lumen-installer=~1.0"
